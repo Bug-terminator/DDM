60f0a16cab2520fc5ff233cec781716de31baeac
ak <litangusa@gmail.com>
2023-01-04 09:46:07 +0800

    update data embedding

--- Document-similarity-k-shingles-minhash/data-embedding/data-embedding.py
+++ Document-similarity-k-shingles-minhash/data-embedding/data-embedding.py
@@ -1,39 +1,54 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from torchtext import data
 
-# Define the RNN model
+# Set device for training
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Define the text field and build the vocabulary
+text_field = data.Field(sequential=True, lower=True, fix_length=200)
+fields = [('text', text_field)]
+dataset = data.TabularDataset(path='dataset.tsv', format='tsv', fields=fields)
+text_field.build_vocab(dataset, min_freq=2)
+
+# Create the iterator and set the batch size
+iterator = data.BucketIterator(dataset, batch_size=32, device=device)
+
+# Define the model
 class RNNEmbedding(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, vocab_size, embedding_size, hidden_size, output_size):
         super().__init__()
-        self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        self.lstm = nn.LSTM(embedding_size, hidden_size, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
     
     def forward(self, x):
-        # x should have shape (batch_size, seq_length, input_size)
-        output, _ = self.rnn(x)
+        # x has shape (batch_size, seq_length)
+        x = self.embedding(x)
+        # x has shape (batch_size, seq_length, embedding_size)
+        output, _ = self.lstm(x)
         # output has shape (batch_size, seq_length, hidden_size)
         output = self.fc(output)
         # output has shape (batch_size, seq_length, output_size)
         return output
 
 # Instantiate the model
-input_size = 128
+vocab_size = len(text_field.vocab)
+embedding_size = 100
 hidden_size = 256
 output_size = 64
-model = RNNEmbedding(input_size, hidden_size, output_size)
+model = RNNEmbedding(vocab_size, embedding_size, hidden_size, output_size).to(device)
 
 # Define the loss function and optimizer
 criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters())
+optimizer = optim.Adam(model.parameters())
 
 # Train the model
 for epoch in range(num_epochs):
-    for data, labels in train_loader:
-        data = data.to(device)
-        labels = labels.to(device)
-
+    for batch in iterator:
         optimizer.zero_grad()
-        output = model(data)
+        output = model(batch.text)
         loss = criterion(output, labels)
         loss.backward()
         optimizer.step()
